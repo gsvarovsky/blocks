33e0cac6572ff0353909dfd2f0e0a736e1c8f473
George Svarovsky <gsvarovsky@gmail.com>
2018-01-20 18:28:48 +0000

    Added full usage example

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ _a blockchain building blocks library_
 Implement [Block](/src/main/java/org/m_ld/block/Block.java), or use the provided [SHA-256 UUID-based chain block](/src/main/java/org/m_ld/block/uuid/UuidChainBlock.java).
 
 Then choose whether to implement [BlockChain](/src/main/java/org/m_ld/block/BlockChain.java), use the provided [InMemoryBlockChain](/src/main/java/org/m_ld/block/InMemoryBlockChain.java), or roll your own container class.
+
+[Here is a full usage example](/src/test/java/org/m_ld/block/uuid/UuidBlockChainTest.java).
---
+++ src/test/java/org/m_ld/block/uuid/UuidBlockChainTest.java
@@ -0,0 +1,51 @@
+package org.m_ld.block.uuid;
+
+import org.junit.Test;
+import org.m_ld.block.InMemoryBlockChain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * An overall usage example
+ */
+public class UuidBlockChainTest
+{
+    static class Data implements Serializable
+    {
+        final long timestamp;
+        final String creator;
+        final String payload;
+
+        Data(String creator, String payload)
+        {
+            this.payload = payload;
+            this.timestamp = new Date().getTime();
+            this.creator = creator;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Data{timestamp=" + timestamp + ", creator='" + creator + '\'' + ", payload='" + payload + "'}";
+        }
+    }
+
+    @Test
+    public void testOverallUsage()
+    {
+        final InMemoryBlockChain<UUID, Data> blockChain = new InMemoryBlockChain<>(UuidChainBlock.<Data>genesis());
+        blockChain.add(new Data("gsvarovsky", "proof-of-work 1"));
+        blockChain.add(new Data("gsvarovsky", "proof-of-work 2"));
+
+        // Not really necessary if I've just created it, but it feels good
+        blockChain.verify();
+
+        System.out.println("Data:");
+        blockChain.forEach(System.out::println);
+
+        System.out.println("Blocks:");
+        blockChain.blocks().forEach(System.out::println);
+    }
+}
